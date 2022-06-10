df4644b94e66448b7b61d730d0dd9a32f16aea06
Shixuan Fang <shixuanf@usc.edu>
2022-06-09 21:16:17 -0700

    6.9

---
+++ PhantomEngineRuntime/Graphics.cpp
@@ -0,0 +1,10 @@
+#include "Graphics.h"
+
+void D3D12HelloWorld::OnInit() {
+	LoadPipeline();
+	LoadAssets();
+}
+
+void D3D12HelloWorld::LoadPipeline() {
+	DX::ThrowIfFailed()
+}
---
+++ PhantomEngineRuntime/Graphics.h
@@ -0,0 +1,91 @@
+#pragma once
+#include <d3d12.h>
+#include <wrl.h>
+#include <string>
+#include <Windows.h>
+#include <dxgi1_4.h>
+
+
+namespace wrl = Microsoft::WRL;
+using namespace wrl;
+
+
+
+/*should be in pch.h*/
+#include <exception>
+namespace DX
+{
+    // Helper class for COM exceptions
+    class com_exception : public std::exception
+    {
+    public:
+        com_exception(HRESULT hr) : result(hr) {}
+
+        const char* what() const override
+        {
+            static char s_str[64] = {};
+            sprintf_s(s_str, "Failure with HRESULT of %08X",
+                static_cast<unsigned int>(result));
+            return s_str;
+        }
+
+    private:
+        HRESULT result;
+    };
+
+    // Helper utility converts D3D API failures into exceptions.
+    inline void ThrowIfFailed(HRESULT hr)
+    {
+        if (FAILED(hr))
+        {
+            throw com_exception(hr);
+        }
+    }
+}
+
+
+
+
+/*reference for ComPtr
+/https://www.youtube.com/watch?v=sxt_pshuW3E
+*/
+
+class D3D12HelloWorld {
+public:
+	D3D12HelloWorld(UINT width, UINT height, std::wstring name);
+    virtual void OnInit();
+    virtual void OnUpdate();
+    virtual void OnRender();
+    virtual void OnDestroy();
+private:
+    static const UINT FrameCount = 2;
+
+    // Pipeline objects.
+    D3D12_VIEWPORT m_viewport;
+    D3D12_RECT m_scissorRect;
+    ComPtr<IDXGISwapChain3> m_swapChain;
+    ComPtr<ID3D12Device> m_device;
+    ComPtr<ID3D12Resource> m_renderTargets[FrameCount];
+    ComPtr<ID3D12CommandAllocator> m_commandAllocator;
+    ComPtr<ID3D12CommandQueue> m_commandQueue;
+    ComPtr<ID3D12RootSignature> m_rootSignature;
+    ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+    ComPtr<ID3D12PipelineState> m_pipelineState;
+    ComPtr<ID3D12GraphicsCommandList> m_commandList;
+    UINT m_rtvDescriptorSize;
+
+    // App resources.
+    ComPtr<ID3D12Resource> m_vertexBuffer;
+    D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+
+    // Synchronization objects.
+    UINT m_frameIndex;
+    HANDLE m_fenceEvent;
+    ComPtr<ID3D12Fence> m_fence;
+    UINT64 m_fenceValue;
+
+    void LoadPipeline();
+    void LoadAssets();
+    void PopulateCommandList();
+    void WaitForPreviousFrame();
+};
